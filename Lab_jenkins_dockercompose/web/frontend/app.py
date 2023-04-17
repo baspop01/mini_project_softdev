9dbef6e4eae16a56da86d3f2f9c1a3e1ae228a74
baspop01 <bas_sos_2011@hotmail.com>
2023-04-17 19:53:34 +0700

    change code ให้ run flask

--- Lab_jenkins_dockercompose/web/frontend/app.py
+++ Lab_jenkins_dockercompose/web/frontend/app.py
@@ -16,8 +16,15 @@ app = Flask(__name__,template_folder="")
 
 @app.route('/')
 def template():
-
     return render_template("index.html")    
+
+@app.route('/data.html')
+def home_path():
+    return render_template("data.html")
+
+@app.route('/about_us.html')
+def CalElectic():
+    return render_template("about_us.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port="8081")
